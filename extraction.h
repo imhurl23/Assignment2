d4c9a6137ceb9061444b00b0d6c0ee0e889bec36
imhurl23 <imhurl23@colby.edu>
2022-03-08 10:30:04 +0200

    adding header parsing function for .pgm files

--- extraction.h
+++ extraction.h
@@ -1,1 +1,12 @@
-extraction.h
+#ifndef Extraction_H
+#define Extraction_H
+
+namespace HRLISA004 {
+
+	//function to read through commend lines and get the width and height of the pgm referenced by imgfile ptr 
+	std::tuple<int, int> parseHeaderInfo(std::ifstream & imgfileptr);
+
+	//function to get 
+	unsigned char ** getBinaryData(std::ifstream & imgfileptr);
+ }
+#endif
--- extractor.cpp
+++ extractor.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <tuple>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -6,16 +7,53 @@
 #include <cstring>
 #include <sstream>
 #include "frame_sequence.h"
+#include "extraction.h"
+
+
 
 int main(int argc, char *argv[]){
 	std::string filename;
-	if (argc < 2) {filename << argv[1]; }
-	 
-	for (int i = 0; i < argc; i++){
-		std::cout << argv[i] << std::endl;
+	if (argc > 2) {filename = argv[1]; }
+	else {
+		filename = "examples/larger_image.pgm";
 	}
+	
+
+	std::ifstream imgfile(filename, std::ios::binary); // open file in binary mode
+	if (!imgfile) {
+	std::cerr << "File open failed!" << std::endl;
+	} 
+
+	HRLISA004::parseHeaderInfo(imgfile);
+	
+	
+
+	return 0;}
 
 
-	ifstream myfile(filename, ios::binary); // open file in binary mode
-	return 0;
+ std::tuple<int, int> HRLISA004::parseHeaderInfo(std::ifstream & imgfileptr){
+	char array[100];
+	std::string str;
+
+	getline(imgfileptr,str);
+	if ( str!= "P5") {
+		std::cerr << "File format incorrect" << std::endl;
+	}
+	while (getline(imgfileptr,str)){
+		if (str[0] != '#'){
+			std::cout << "BREAK" << std::endl;
+			break;
+		}
+		std::cout << "The next line is " << str << std::endl;
+	}
+	int width, height; 
+	std::istringstream iss(str);
+	iss >> width >> std::ws; 
+	iss >> height >> std::ws;
+	std::cout << width <<  "," << height << std::endl;
+	return std::make_tuple(width,height);
+
 }
+
+
+
